4fb49236bed934873682c6840c689b8d5069fa1a
Thomas Erbesdobler <erbesdob@cit.tum.de>
2025-04-18 23:21:16 +0200

    added timerfd from sdfs

---
+++ projects/mpitofino/common/timerfd.h
@@ -0,0 +1,34 @@
+/* This file was copied from sdfs, available from https://github.com/erbth/sdfs
+ * under the MIT license. */
+#ifndef __COMMON_TIMERFD_H
+#define __COMMON_TIMERFD_H
+
+#include <functional>
+#include "common/utils.h"
+#include "common/epoll.h"
+
+class TimerFD final
+{
+public:
+	using cb_t = std::function<void()>;
+
+protected:
+	Epoll& epoll;
+	WrappedFD wfd;
+
+	cb_t cb;
+
+	void on_fd(int fd, uint32_t events);
+
+public:
+	TimerFD(Epoll& epoll, cb_t cb);
+	~TimerFD();
+
+	/* interval is measured in microseconds */
+	void start(unsigned long interval);
+
+	/* Missed events of a stopped TimerFD will still be executed */
+	void stop();
+};
+
+#endif /* __COMMON_TIMERFD_H */
