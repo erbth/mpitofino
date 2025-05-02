765eda5384b5683b61317116b07587511a982dd7
Thomas Erbesdobler <erbesdob@cit.tum.de>
2025-05-02 02:46:56 +0200

    use correct fp port numbers for loopback ports on hw

--- projects/tools/enable_ports_stordis2.sh
+++ projects/tools/enable_ports_stordis2.sh
@@ -8,17 +8,17 @@ port-add 13/0 100G RS
 port-add 14/0 100G RS
 port-add 15/0 100G RS
 port-add 65/0 10G none
-port-add 60/0 100g none
-port-add 64/0 100g none
-port-loopback 60/0 serdes-near
-port-loopback 64/0 serdes-near
+port-add 4/0 100g none
+port-add 3/0 100g none
+port-loopback 4/0 serdes-near
+port-loopback 3/0 serdes-near
 port-enb 12/0
 port-enb 13/0
 port-enb 14/0
 port-enb 15/0
 port-enb 65/0
-port-enb 60/0
-port-enb 64/0
+port-enb 4/0
+port-enb 3/0
 show
 exit
 exit
